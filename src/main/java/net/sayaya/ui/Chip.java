d9ab4a7f63020348845550e71ee34d06ad77e7d0
변상재 <sayaya@greencross.com>
2020-11-12 21:48:46 +0900

    TextArea implemented

--- src/main/java/net/sayaya/ui/Chip.java
+++ src/main/java/net/sayaya/ui/Chip.java
@@ -1,9 +1,15 @@
 package net.sayaya.ui;
 
+import com.google.gwt.core.client.Scheduler;
 import elemental2.dom.*;
 import elemental2.svg.SVGElement;
 import elemental2.svg.SVGPathElement;
+import jsinterop.annotations.JsOverlay;
+import jsinterop.annotations.JsPackage;
+import jsinterop.annotations.JsType;
 import jsinterop.base.JsPropertyMap;
+import lombok.Setter;
+import lombok.experimental.Accessors;
 import net.sayaya.ui.Animation;
 import net.sayaya.ui.event.HasAttachHandlers;
 import net.sayaya.ui.event.HasDetachHandlers;
@@ -19,15 +25,25 @@ import static org.jboss.elemento.EventType.bind;
 public class Chip extends HTMLElementBuilder<HTMLDivElement, Chip> implements HasAttachHandlers, HasDetachHandlers {
 	public static Chip chip(String text) {
 		Chip elem = new Chip(div().css("mdc-chip").attr("role", "row")).text(text);
-		bind(elem, "DOMNodeInserted", evt->inject(elem.element()));
+		bind(elem, "DOMNodeInserted", evt->{
+			elem._mdc = inject(elem.element());
+		});
 		return elem;
 	}
-	private static native void inject(Element elem) /*-{
-        $wnd.mdc.chips.MDCChip.attachTo(elem);
+	public static ChipCheckable check(String text) {
+		ChipCheckable elem = new ChipCheckable(div().css("mdc-chip").attr("role", "row"));
+		elem.text(text);
+		bind(elem, "DOMNodeInserted", evt->{
+			((Chip)elem)._mdc = inject(elem.element());
+			((Chip)elem)._mdc.setSelectedFromChipSet(elem.value);
+		});
+		return elem;
+	}
+	private static native MdcChip inject(Element elem) /*-{
+        return $wnd.mdc.chips.MDCChip.attachTo(elem);
     }-*/;
 	private final HtmlContentBuilder<HTMLDivElement> ripple = div().css("mdc-chip__ripple");
 	private IsElement<?> iconBefore;
-	private IsElement<?> check;
 	private final HtmlContentBuilder<HTMLElement> label = span().css("mdc-chip__text");
 	private final HtmlContentBuilder<HTMLElement> btn = span().css("mdc-chip__primary-action").attr("role", "checkbox").attr("tabindex", "0")
 															  .add(label);
@@ -35,19 +51,16 @@ public class Chip extends HTMLElementBuilder<HTMLDivElement, Chip> implements Ha
 															   .add(btn);
 	private IsElement<?> iconTrailing;
 	private final HtmlContentBuilder<HTMLDivElement> _this;
+	private MdcChip _mdc;
 	private Chip(HtmlContentBuilder<HTMLDivElement> e) {
 		super(e);
 		_this = e;
 		layout();
 	}
-	private void layout() {
+	protected void layout() {
 		clear();
 		_this.add(ripple);
 		if(iconBefore!=null) _this.add(iconBefore);
-		if(check!=null) {
-			_this.add(check);
-			if(iconBefore!=null) iconBefore.element().classList.add("mdc-chip__icon--leading");
-		}
 		_this.add(cell);
 		if(iconTrailing!=null) _this.add(iconTrailing);
 	}
@@ -81,27 +94,51 @@ public class Chip extends HTMLElementBuilder<HTMLDivElement, Chip> implements Ha
 		return this;
 	}
 	private final static String SVG_NAMESPACE = "http://www.w3.org/2000/svg";
-	public Chip checkable() {
-		check = span().css("mdc_chip__checkmark").add(checkmark());
-		layout();
-		return this;
-	}
-	private static SVGElement checkmark() {
-		SVGPathElement path = (SVGPathElement) DomGlobal.document.createElementNS(SVG_NAMESPACE, "path");
-		path.classList.add("mdc-chip__checkmark-path");
-		path.setAttribute("d", "M1.73,12.91 8.1,19.28 22.79,4.59");
-		path.setAttribute("fill", "none");
-		path.setAttribute("stroke", "black");
+	public static class ChipCheckable extends Chip {
+		private final IsElement<?> check = span().css("mdc_chip__checkmark").add(checkmark());
+		private boolean value = false;
+		private ChipCheckable(HtmlContentBuilder<HTMLDivElement> e) {
+			super(e);
+		}
+		@Override
+		protected void layout() {
+			clear();
+			super._this.add(super.ripple);
+			if(super.iconBefore!=null) super._this.add(super.iconBefore);
+			super._this.add(check);
+			super._this.add(super.cell);
+			if(super.iconTrailing!=null) super._this.add(super.iconTrailing);
+		}
+		public ChipCheckable select(boolean select) {
+			value = select;
+			if(super._mdc!=null) super._mdc.setSelectedFromChipSet(select);
+			return that();
+		}
+		@Override
+		public ChipCheckable that() {
+			return this;
+		}
+		private static SVGElement checkmark() {
+			SVGPathElement path = (SVGPathElement) DomGlobal.document.createElementNS(SVG_NAMESPACE, "path");
+			path.classList.add("mdc-chip__checkmark-path");
+			path.setAttribute("d", "M1.73,12.91 8.1,19.28 22.79,4.59");
+			path.setAttribute("fill", "none");
+			path.setAttribute("stroke", "black");
 
-		SVGElement svg = (SVGElement) DomGlobal.document.createElementNS(SVG_NAMESPACE, "svg");
-		svg.classList.add("mdc-chip__checkmark-svg");
-		svg.setAttribute( "viewBox", "-2 -3 30 30");
-		svg.appendChild(path);
-		return svg;
+			SVGElement svg = (SVGElement) DomGlobal.document.createElementNS(SVG_NAMESPACE, "svg");
+			svg.classList.add("mdc-chip__checkmark-svg");
+			svg.setAttribute( "viewBox", "-2 -3 30 30");
+			svg.appendChild(path);
+			return svg;
+		}
 	}
 	@Override
 	public Chip that() {
 		return this;
+	}
+	public Chip removeFocus() {
+		_mdc.removeFocus();
+		return that();
 	}
 	@Override
 	public HandlerRegistration onAttach(EventListener listener) {
@@ -110,5 +147,16 @@ public class Chip extends HTMLElementBuilder<HTMLDivElement, Chip> implements Ha
 	@Override
 	public HandlerRegistration onDetach(EventListener listener) {
 		return onDetach(element(), listener);
+	}
+
+	@JsType(isNative = true, namespace= JsPackage.GLOBAL, name="Object")
+	@Setter(onMethod_ = {@JsOverlay})
+	@Accessors(fluent=true)
+	private static final class MdcChip {
+		public native void beginExit();
+		public native void focusPrimaryAction();
+		public native void focusTrailingAction();
+		public native void removeFocus();
+		public native void setSelectedFromChipSet(boolean selected);
 	}
 }
--- src/main/java/net/sayaya/ui/ChipSet.java
+++ src/main/java/net/sayaya/ui/ChipSet.java
@@ -1,7 +1,13 @@
 package net.sayaya.ui;
 
+import com.google.gwt.core.client.Scheduler;
 import elemental2.dom.Element;
 import elemental2.dom.HTMLDivElement;
+import jsinterop.annotations.JsOverlay;
+import jsinterop.annotations.JsPackage;
+import jsinterop.annotations.JsType;
+import lombok.Setter;
+import lombok.experimental.Accessors;
 import net.sayaya.ui.event.HasValueChangeHandlers;
 import org.gwtproject.event.shared.HandlerRegistration;
 import org.jboss.elemento.HtmlContentBuilder;
@@ -15,37 +21,47 @@ import static org.jboss.elemento.EventType.bind;
 public class ChipSet extends HTMLElementBuilder<HTMLDivElement, ChipSet> implements HasValueChangeHandlers<Chip[]> {
 	public static ChipSet chips() {
 		ChipSet elem =  new ChipSet(div().css("mdc-chip-set").attr("role", "grid"));
-		bind(elem, "DOMNodeInserted", evt->inject(elem.element()));
+		bind(elem, "DOMNodeInserted", evt->{
+			elem._mdc = inject(elem.element());
+		});
 		return elem;
 	}
 	public static ChipSet choices() {
 		ChipSet elem =  new ChipSet(div().css("mdc-chip-set", "mdc-chip-set--choice").attr("role", "grid"));
-		bind(elem, "DOMNodeInserted", evt->inject(elem.element()));
+		bind(elem, "DOMNodeInserted", evt->{
+			elem._mdc = inject(elem.element());
+		});
 		return elem;
 	}
-	public static ChipSet filters(Chip... chips) {
+	public static ChipSet filters(Chip.ChipCheckable... chips) {
 		ChipSet elem =  new ChipSet(div().css("mdc-chip-set", "mdc-chip-set--filter").attr("role", "grid"));
 		for(Chip c: chips) elem.add(c);
-		bind(elem, "DOMNodeInserted", evt->inject(elem.element()));
+		bind(elem, "DOMNodeInserted", evt->{
+			elem._mdc = inject(elem.element());
+		});
 		return elem;
 	}
-	native static void inject(Element elem) /*-{
-        $wnd.mdc.chips.MDCChipSet.attachTo(elem);
+	native static MdcChipSet inject(Element elem) /*-{
+        return $wnd.mdc.chips.MDCChipSet.attachTo(elem);
     }-*/;
 	private final Set<ValueChangeEventListener<Chip[]>> listeners = new HashSet<>();
 	private final Set<Chip> chips = new HashSet<>();
 	private final HtmlContentBuilder<HTMLDivElement> _this;
+	private MdcChipSet _mdc;
 	private ChipSet(HtmlContentBuilder<HTMLDivElement> element) {
 		super(element);
 		_this = element;
 
 	}
 	public final ChipSet add(final Chip chip) {
-		_this.add(chip);
+		if(_mdc!=null) _mdc.addChip(chip.element());
+		else _this.add(chip);
 		chips.add(chip);
-		/*chip.onDetach(evt->{
-			chips.remove(chip);
-			fire(ValueChangeEvent.event(evt, value()));
+		/*Scheduler.get().scheduleDeferred(()-> {
+			chip.onDetach(evt -> {
+				chips.remove(chip);
+				fire(ValueChangeEvent.event(evt, value()));
+			});
 		});*/
 		return this;
 	}
@@ -71,5 +87,12 @@ public class ChipSet extends HTMLElementBuilder<HTMLDivElement, ChipSet> impleme
 	@Override
 	public ChipSet that() {
 		return this;
+	}
+
+	@JsType(isNative = true, namespace= JsPackage.GLOBAL, name="Object")
+	@Setter(onMethod_ = {@JsOverlay})
+	@Accessors(fluent=true)
+	private static final class MdcChipSet {
+		public native void addChip(Element elem);
 	}
 }
